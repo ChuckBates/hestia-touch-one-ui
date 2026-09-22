e6f9519164e1be8a6e2987c933468cb01f5ab7c0
Chuck Bates <8691839+ChuckBates@users.noreply.github.com>
2026-09-22 11:08:36 -0600

    Modern kiosk spike
    * update to modern browser (surf)
    
    * remove redundant comment

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig(() => ({
+  base: './',
   build: {
-    minify: false
+    minify: false,
   },
   define: {
-    MQTT_SERVER: JSON.stringify(process.env.MQTT_SERVER || 'localhost')
+    MQTT_SERVER: JSON.stringify(process.env.MQTT_SERVER || 'localhost'),
   },
   plugins: [
-    vue()
+    vue(),
+    nodePolyfills(),
+    viteSingleFile(),
   ],
   server: {
     host: true,
-    port: 8080
+    port: 8080,
   },
-  target: 'es2015'
 }))
-
